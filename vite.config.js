c87b647acab3f27c98e80a526f1d0260bcdbd613
leonnnnnneeee <leon@coincu.com>
2026-06-05 17:06:30 +0700

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
-  server: { port: process.env.PORT || 3000, host: true }
+  build: { outDir: 'dist' },
+  server: { port: 3000, host: true }
 })
